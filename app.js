fc93b89e7308bff1f0835a0e7ea0434eb5999f50
Milanesa <diegojara88574@gmail.com>
2023-07-05 14:27:22 -0300

    Completado el codigo faltante en el archivo 'app,js', ademas de finalizacion del archivo .env para poder implementar variables de entorno

--- app.js
+++ app.js
@@ -1,20 +1,54 @@
 // Imports
-const cors = require('cors');
-const express = require('express');
+const express = require("express");
 
-const path = require('path');
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const path = require("path");
 
 const app = express();
+//configuración de variables de entorno
+dotenv.config({ path: ".env" });
+const { sequelize } = require('./database/database');
+
+
+
+//configuración del motor de plantillas
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 
 // Middlewares
 // TODO: Implementar middlewares
+app.use(cors());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+app.use(morgan("dev"));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+// carpeta para archivos estáticos
+app.use(express.static(path.join(__dirname, "public")));
+
+
+
 
 // Routes
-app.use('/api', require('./routes/reserva.routes'));
+app.use("/", require("./routes/reserva.routes"));
+
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
+// 404 - Not found
+app.use((_req, res, next) => {
+    res.render('errors/404')
+  });
 
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+app.listen(process.env.PORT, () => console.log(`Servidor en http://localhost:${process.env.PORT}`));
